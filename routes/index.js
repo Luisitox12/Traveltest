4472dde5d034d8683dfd8460697ffae764d0c00b
Luisitox12 <luishidalgops6@gmail.com>
2025-05-08 20:35:02 -0400

    bug solucion

--- routes/index.js
+++ routes/index.js
@@ -62,6 +62,381 @@ db.serialize(() => {
   )`);
 });
 
+// Inserción de datos en la tabla respuestas
+router.post('/submit', function(req, res, next) {
+  const r = req.body;
+  const fechaRegistro = new Date().toISOString();
+
+  const stmt = db.prepare(`INSERT INTO respuestas (
+    nombreCompleto, fechaActual, edadEncuestado,
+    pregunta1, pregunta2, pregunta2_comentarios, pregunta3, pregunta4, pregunta5, pregunta5_otro,
+    pregunta6, pregunta6_comentarios, pregunta7, pregunta8, pregunta8_comentarios, pregunta9,
+    pregunta10, pregunta10_comentarios, pregunta11, pregunta11_comentarios, pregunta12, pregunta12_comentarios,
+    pregunta13, pregunta14, pregunta14_comentarios, pregunta15, pregunta16, pregunta17, pregunta17_comentarios,
+    pregunta18, pregunta18_comentarios, pregunta19, pregunta19_comentarios, pregunta20, pregunta20_comentarios,
+    pregunta21, pregunta22, pregunta22_comentarios, pregunta23, pregunta23_comentarios, pregunta24, pregunta25,
+    pregunta26, pregunta27, pregunta28, fechaRegistro
+  ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
+
+  stmt.run(
+    r.nombreCompleto, r.fechaActual, r.edadEncuestado,
+    r.pregunta1, r.pregunta2, r.pregunta2_comentarios, r.pregunta3, r.pregunta4, r.pregunta5, r.pregunta5_otro,
+    r.pregunta6, r.pregunta6_comentarios, r.pregunta7, r.pregunta8, r.pregunta8_comentarios, r.pregunta9,
+    r.pregunta10, r.pregunta10_comentarios, r.pregunta11, r.pregunta11_comentarios, r.pregunta12, r.pregunta12_comentarios,
+    r.pregunta13, r.pregunta14, r.pregunta14_comentarios, r.pregunta15, r.pregunta16, r.pregunta17, r.pregunta17_comentarios,
+    r.pregunta18, r.pregunta18_comentarios, r.pregunta19, r.pregunta19_comentarios, r.pregunta20, r.pregunta20_comentarios,
+    r.pregunta21, r.pregunta22, r.pregunta22_comentarios, r.pregunta23, r.pregunta23_comentarios, r.pregunta24, r.pregunta25,
+    r.pregunta26, r.pregunta27, r.pregunta28, fechaRegistro,
+    function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.render('confirmacion');
+    }
+  );
+
+  stmt.finalize();
+});
+
+// Función para renderizar resultados agregados
+function renderResultados(req, res, next) {
+  const preguntas = [
+    'pregunta1', 'pregunta2', 'pregunta3', 'pregunta4', 'pregunta5', 'pregunta6', 'pregunta7',
+    'pregunta8', 'pregunta9', 'pregunta10', 'pregunta11', 'pregunta12', 'pregunta13', 'pregunta14',
+    'pregunta15', 'pregunta16', 'pregunta17', 'pregunta18', 'pregunta19', 'pregunta20', 'pregunta21',
+    'pregunta22', 'pregunta23', 'pregunta24', 'pregunta25', 'pregunta26', 'pregunta27', 'pregunta28'
+  ];
+
+  const query = `
+    SELECT 
+      strftime('%Y-%m', fechaRegistro) AS mes,
+      pregunta,
+      respuesta,
+      COUNT(*) AS total
+    FROM (
+      SELECT 
+        fechaRegistro,
+        'pregunta1' AS pregunta, pregunta1 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta2' AS pregunta, pregunta2 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta2_comentarios' AS pregunta, pregunta2_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta3' AS pregunta, pregunta3 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta4' AS pregunta, pregunta4 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta5' AS pregunta, pregunta5 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta5_otro' AS pregunta, pregunta5_otro AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta6' AS pregunta, pregunta6 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta6_comentarios' AS pregunta, pregunta6_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta7' AS pregunta, pregunta7 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta8' AS pregunta, pregunta8 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta8_comentarios' AS pregunta, pregunta8_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta9' AS pregunta, pregunta9 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta10' AS pregunta, pregunta10 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta10_comentarios' AS pregunta, pregunta10_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta11' AS pregunta, pregunta11 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta11_comentarios' AS pregunta, pregunta11_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta12' AS pregunta, pregunta12 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta12_comentarios' AS pregunta, pregunta12_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta13' AS pregunta, pregunta13 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta14' AS pregunta, pregunta14 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta14_comentarios' AS pregunta, pregunta14_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta15' AS pregunta, pregunta15 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta16' AS pregunta, pregunta16 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta17' AS pregunta, pregunta17 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta17_comentarios' AS pregunta, pregunta17_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta18' AS pregunta, pregunta18 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta18_comentarios' AS pregunta, pregunta18_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta19' AS pregunta, pregunta19 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta19_comentarios' AS pregunta, pregunta19_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta20' AS pregunta, pregunta20 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta20_comentarios' AS pregunta, pregunta20_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta21' AS pregunta, pregunta21 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta22' AS pregunta, pregunta22 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta22_comentarios' AS pregunta, pregunta22_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta23' AS pregunta, pregunta23 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta23_comentarios' AS pregunta, pregunta23_comentarios AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta24' AS pregunta, pregunta24 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta25' AS pregunta, pregunta25 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta26' AS pregunta, pregunta26 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta27' AS pregunta, pregunta27 AS respuesta FROM respuestas
+      UNION ALL
+      SELECT 
+        fechaRegistro,
+        'pregunta28' AS pregunta, pregunta28 AS respuesta FROM respuestas
+    )
+    GROUP BY mes, pregunta, respuesta
+    ORDER BY mes ASC, pregunta ASC
+  `;
+
+  const totalRespuestasQuery = `
+    SELECT 
+      strftime('%Y-%m', fechaRegistro) AS mes,
+      COUNT(*) AS total
+    FROM respuestas
+    GROUP BY mes
+    ORDER BY mes ASC
+  `;
+
+  const comentariosQuery = `
+    SELECT 
+      nombreCompleto,
+      fechaRegistro,
+      pregunta2_comentarios,
+      pregunta6_comentarios,
+      pregunta8_comentarios,
+      pregunta10_comentarios,
+      pregunta11_comentarios,
+      pregunta12_comentarios,
+      pregunta14_comentarios,
+      pregunta17_comentarios,
+      pregunta18_comentarios,
+      pregunta19_comentarios,
+      pregunta20_comentarios,
+      pregunta23_comentarios,
+      pregunta28_comentarios
+    FROM respuestas
+    ORDER BY fechaRegistro DESC
+  `;
+
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      return next(err);
+    }
+
+    const dataPorPregunta = {};
+    const opcionesRespuestaPorPregunta = {};
+    const mesesSet = new Set();
+
+    rows.forEach(row => {
+      mesesSet.add(row.mes);
+      if (!dataPorPregunta[row.pregunta]) {
+        dataPorPregunta[row.pregunta] = {};
+        opcionesRespuestaPorPregunta[row.pregunta] = new Set();
+      }
+      if (!dataPorPregunta[row.pregunta][row.mes]) {
+        dataPorPregunta[row.pregunta][row.mes] = {};
+      }
+      // Normalizar opciones para pregunta2 a "Sí" o "No"
+      let respuestaNormalizada = row.respuesta;
+      if (row.pregunta === 'pregunta2') {
+        if (typeof row.respuesta === 'string') {
+          const val = row.respuesta.trim().toLowerCase();
+          if (val === 'sí' || val === 'si') {
+            respuestaNormalizada = 'Sí';
+          } else if (val === 'no') {
+            respuestaNormalizada = 'No';
+          } else {
+            respuestaNormalizada = row.respuesta;
+          }
+        }
+      }
+      dataPorPregunta[row.pregunta][row.mes][respuestaNormalizada] = row.total;
+      opcionesRespuestaPorPregunta[row.pregunta].add(respuestaNormalizada);
+    });
+
+    const meses = Array.from(mesesSet).sort();
+    Object.keys(dataPorPregunta).forEach(pregunta => {
+      meses.forEach(mes => {
+        if (!dataPorPregunta[pregunta][mes]) {
+          dataPorPregunta[pregunta][mes] = {};
+        }
+        opcionesRespuestaPorPregunta[pregunta].forEach(opcion => {
+          if (!dataPorPregunta[pregunta][mes][opcion]) {
+            dataPorPregunta[pregunta][mes][opcion] = 0;
+          }
+        });
+      });
+    });
+
+    db.all(totalRespuestasQuery, [], (err2, totalRows) => {
+      if (err2) {
+        return next(err2);
+      }
+
+      const totalRespuestasPorMes = {};
+      totalRows.forEach(row => {
+        totalRespuestasPorMes[row.mes] = row.total;
+      });
+
+      db.all(comentariosQuery, [], (err3, comentariosRows) => {
+        if (err3) {
+          return next(err3);
+        }
+
+        res.render('resultados', {
+          title: 'Resultados de la Encuesta',
+          dataPorPregunta,
+          opcionesRespuestaPorPregunta: Object.keys(opcionesRespuestaPorPregunta).reduce((acc, key) => {
+            acc[key] = Array.from(opcionesRespuestaPorPregunta[key]);
+            return acc;
+          }, {}),
+          meses,
+          totalRespuestasPorMes,
+          comentarios: comentariosRows
+        });
+      });
+    });
+  });
+}
+
+router.get('/resultados', checkAdminAuth, renderResultados);
+router.get('/resultados/public', renderResultados);
+
+router.get('/resultados/pdf', async function(req, res, next) {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    const host = req.headers.host;
+    const protocol = req.protocol;
+    const url = `${protocol}://${host}/resultados/public`;
+    console.log('Generando PDF desde URL pública:', url);
+    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
+    await page.waitForFunction(() => {
+      const bars = document.querySelectorAll('.bar-fill');
+      return bars.length > 0 && Array.from(bars).every(bar => bar.offsetWidth > 0);
+    }, { timeout: 60000 });
+    const pdfBuffer = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      landscape: false
+    });
+    await browser.close();
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename="resultados.pdf"',
+      'Content-Length': pdfBuffer.length
+    });
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error('Error generando PDF:', error);
+    next(error);
+  }
+});
+
+module.exports = router;
+
 // Usuario administrador local (usuario y contraseña hasheada)
 const adminUser = {
   username: process.env.ADMIN_USER || 'admin',
@@ -112,6 +487,7 @@ router.get('/admin/logout', function(req, res, next) {
 
 router.post('/submit', function(req, res, next) {
   const r = req.body;
+  console.log('Contenido de pregunta10_comentarios:', r.pregunta10_comentarios); // Log para diagnosticar
   const fechaRegistro = new Date().toISOString();
 
   const stmt = db.prepare(`INSERT INTO respuestas (
@@ -328,8 +704,13 @@ function renderResultados(req, res, next) {
       if (!dataPorPregunta[row.pregunta][row.mes]) {
         dataPorPregunta[row.pregunta][row.mes] = {};
       }
-      dataPorPregunta[row.pregunta][row.mes][row.respuesta] = row.total;
-      opcionesRespuestaPorPregunta[row.pregunta].add(row.respuesta);
+      // Normalizar respuesta para pregunta2 para evitar problemas de espacios o mayúsculas
+      let respuestaNormalizada = row.respuesta;
+      if (row.pregunta === 'pregunta2' && typeof row.respuesta === 'string') {
+        respuestaNormalizada = row.respuesta.trim().toLowerCase();
+      }
+      dataPorPregunta[row.pregunta][row.mes][respuestaNormalizada] = row.total;
+      opcionesRespuestaPorPregunta[row.pregunta].add(respuestaNormalizada);
     });
 
     // Asegurar que para cada pregunta y mes todas las opciones tengan un valor, aunque sea 0
